5293bcf90081d3f5bcd10c5eb14615107e28c957
MeenIon-i <pisitsaman@gmail.com>
2025-08-06 15:48:50 +0700

    fix

---
+++ index.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "กรุณาใส่ชื่อของคุณ: ";
+    cin >> name;
+    cout << "สวัสดี " << name << "!" << endl;
+    return 0;
+}
